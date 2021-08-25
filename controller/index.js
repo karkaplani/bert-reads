74f4db2c184bc20e2de9b03a1be1509515de94f4
karkaplani <ilgu0001@algonquinlive.com>
2021-08-25 19:19:25 -0400

    Python script connection made

--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const {spawn} = require('child_process')
 const app = express()
-const port = 5000
+
+const cors = require('cors')
+
+const {spawn} = require('child_process')
+
 const fs = require('fs')
 var multer = require('multer')
-const cors = require('cors')
+
+const port = 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -12,6 +16,21 @@ app.use(cors())
 
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
+
+let result = ''
+
+const calculateScore = (text) => {
+   return new Promise((resolve, reject) => {
+      const python = spawn('python', ['../BERT/run_finetuned.py', text]);   
+      python.stdout.on('data', (data) => {
+         result = data
+      })
+      python.on('close', (code) => {
+         console.log(`child process close all stdio with code ${code}`)
+         resolve()
+      })
+   })
+}
 
 const storage = multer.diskStorage({
    destination: (req, file, cb) => {
@@ -26,34 +45,19 @@ const upload = multer({storage}).single('file')
 
 app.post('/api/uploads', (req, res) => {
 
-   // console.log(req.file)
    upload(req, res, (err) => {
       if(err) {
          return res.status(500).json(err)
       }
-
       fs.readFile('public/fileToRead', 'utf8', (err, data) => {
          if(err) {
             console.log('Error: ' + err)
             return
          }
-         console.log(data)
-         return res.status(200).send(data)
+         calculateScore(data)
+            .then(() => {
+               return res.status(200).send(result)
+            })
       })
-      // return res.status(200).send(req.file)
    })
 })
-
-// app.post('/api/upload', (req, res) => {
-//     const python = spawn('python', ['../BERT/run_finetuned.py', JSON.stringify(req.body.text)]);   
-//     console.log(JSON.stringify(req.body.text)) 
-//     python.stdout.on('data', (data) => {
-//       console.log('Pipe data from python script ...');
-//       const result = data.toString();
-//       res.send(data);
-//     });
-//     python.on('close', (code) => {
-//     //    console.log(`child process close all stdio with code ${code}`);
-//        res.status(500).end();
-//     });
-// });
